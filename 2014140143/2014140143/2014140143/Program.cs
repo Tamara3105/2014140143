617e87275ae86f1a412185cf94cae6593947ca4a
Tamara3105 <roxana_tamara@usmp.pe>
2017-04-19 03:10:26 -0500

    cambios Pc1

--- 2014140143/2014140143/2014140143/Program.cs
+++ 2014140143/2014140143/2014140143/Program.cs
@@ -10,6 +10,27 @@ namespace _2014140143
     {
         static void Main(string[] args)
         {
+            var evaluacion = new Evaluacion();
+          
+            evaluacion.AgregarCliente("Jordan Espiritu Martinez", "45632189", "2000");
+            evaluacion.AgregarCliente("Jeyson Saavedra Chumpitaz", "45000189", "3000");
+            evaluacion.AgregarCliente("Judhisa Artica Figueroa", "62563219", "2800");
+            evaluacion.AgregarCliente("Neyba Prado Revilla", "78563209", "3500");
+            evaluacion.AgregarCliente("Marcela Miranda Flores", "55775189", "1500");
+            evaluacion.AgregarTipoPago("Efectivo");
+            evaluacion.AgregarContrato("12 meses");
+            evaluacion.AgregarContrato("18 meses");
+            evaluacion.AgregarTipoTrabajador("Asesor");
+           
+
+
+
+
+
+
+
+
+            Console.WriteLine("Holi");
         }
     }
 }
